d237a42fe60e7de6ac864097da5a9d03c6671f26
Roman Averchenkov <aragentum@gmail.com>
2020-06-15 19:49:13 +0300

    updated logging config

--- web/settings/development.py
+++ web/settings/development.py
@@ -23,22 +23,38 @@ DATABASES = {
 
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
+    'disable_existing_loggers': True,
     'filters': {
         'require_debug_false': {
             '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+        'console_formatter': {
+            'format': '[%(asctime)s]:[%(process)d] | [%(levelname)s] | %(name)-15s - %(message)s'
         }
     },
     'handlers': {
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
+            'formatter': 'console_formatter'
         },
     },
     'loggers': {
-        'django.db.backends': {
-            'level': 'DEBUG',
+        '': {
             'handlers': ['console'],
+            'level': 'INFO'
         },
+        'web': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False
+        }
     }
 }
